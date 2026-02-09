aeed7b0964ba5a84f055fa724ee7b32f2a2445c0
Gordon Fraser <gordon.fraser@uni-passau.de>
2026-02-09 16:58:33 +0300

    Fix Checkstyle JavadocParagraph violations in runtime annotations (#197)
    Corrected Javadoc formatting in `org.evosuite.runtime.annotation` package to comply with `JavadocParagraph` checkstyle rule.
    - Added missing empty lines before `<p>` tags.
    - Removed invalid newlines after `<p>` tags.
    - Affected files:
      - `EvoSuiteExclude.java`
      - `EvoSuiteAssertionOnly.java`
      - `EvoSuiteClassExclude.java`
      - `EvoSuiteInclude.java`
    
    Co-authored-by: gofraser <1844172+gofraser@users.noreply.github.com>

--- runtime/src/main/java/org/evosuite/runtime/annotation/EvoSuiteAssertionOnly.java
+++ runtime/src/main/java/org/evosuite/runtime/annotation/EvoSuiteAssertionOnly.java
@@ -24,8 +24,8 @@ import java.lang.annotation.*;
 /**
  * Specify that the tagged method can only be used during assertion generation.
  * It must be pure.
- * <p>
- * Created by Andrea Arcuri on 22/05/15.
+ *
+ * <p>Created by Andrea Arcuri on 22/05/15.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
