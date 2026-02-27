bb9460d11b47465375ae1fc1229b71c40b3f064f
Gordon Fraser <gordon.fraser@uni-passau.de>
2026-02-27 20:28:05 +0100

    Update license header

--- client/src/main/java/org/evosuite/testcase/variable/name/TypeBasedVariableNameStrategy.java
+++ client/src/main/java/org/evosuite/testcase/variable/name/TypeBasedVariableNameStrategy.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2010-2018 Gordon Fraser, Andrea Arcuri and EvoSuite
+/**
+ * Copyright (C) 2010-2026 Gordon Fraser, Andrea Arcuri and EvoSuite
  * contributors
  *
  * This file is part of EvoSuite.
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with EvoSuite. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.evosuite.testcase.variable.name;
 
 import org.apache.commons.lang3.CharUtils;
